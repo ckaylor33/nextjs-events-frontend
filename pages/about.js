d13c7b250ea195712d5815482944a3e10665ef9c
ckaylor33 <kaylor33@hotmail.co.uk>
2021-04-21 20:59:44 +0100

    adding pages and routing

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        This is an app to discover the best tours in the South West of England.
+      </p>
+      <>Version: 1.0.0</>
+    </div>
+  )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddEventPage() {
+  return (
+    <div>
+      <h1>Add Tour</h1>
+    </div>
+  )
+}
